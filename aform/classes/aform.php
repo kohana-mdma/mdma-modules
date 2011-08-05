b295403f83de397f3c8269c1977e318663c0102d
Maxim <Big_Shark@mail.ru>
2011-08-05 14:39:53 +1100

    add function textarea to aform

--- aform/classes/aform.php
+++ aform/classes/aform.php
@@ -6,5 +6,10 @@ class AForm {
 	{
 		return View::factory('aform/default/input', array('name' => $name, 'value' => $value, 'title' => ($title)?$title:$name, 'id'=>($id)?$id:'id'.time()));
 	}
+	
+	public static function textarea($name, $value = NULL, $title = NULL, $id = NULL, $attr = array())
+	{
+		return View::factory('aform/default/textarea', array('name' => $name, 'value' => $value, 'title' => ($title)?$title:$name, 'id'=>($id)?$id:'id'.time(), 'attr'=> $attr));
+	}
 }
 ?>
