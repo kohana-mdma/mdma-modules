c3cb100981b2217272b63450e4e320cacf87b922
Big_Shark <Big_Shark@mail.ru>
2011-09-02 15:14:39 +1100

    update media module

--- media/classes/mdma/controller/media.php
+++ media/classes/mdma/controller/media.php
@@ -2,20 +2,40 @@
 
 class MDMA_Controller_Media extends Controller {
 	
-	private $_console = NULL;
-	
-	public function before(){
-		$this->_console = Console::instance();
-		parent::before();
-	}
-	
-	public function action_index(){
-		$this->_console->out(Console::format("No command.", Console::ERROR).PHP_EOL);
-	}
-	
 	public function action_move()
 	{
 		MDMA_Media::factory()->move();
+	}
+	
+	public function action_get()
+	{
+		// Get the file path from the request
+		$file = $this->request->param('file');
+        $dir = $this->request->param('dir');
+
+		// Find the file extension
+		$ext = pathinfo($file, PATHINFO_EXTENSION);
+
+		// Remove the extension from the filename
+		$file = substr($file, 0, -(strlen($ext) + 1));
+		$file = Kohana::find_file('media', $dir.'/'.$file, $ext);
+		if ($file)
+		{
+			// Check if the browser sent an "if-none-match: <etag>" header, and tell if the file hasn't changed
+			$this->response->check_cache(sha1($this->request->uri()).filemtime($file), $this->request);
+
+			// Send the file content as the response
+			$this->response->body(file_get_contents($file));
+
+			// Set the proper headers to allow caching
+			$this->response->headers('content-type',  File::mime_by_ext($ext));
+			$this->response->headers('last-modified', date('r', filemtime($file)));
+		}
+		else
+		{
+			// Return a 404 status
+			$this->response->status(404);
+		}
 	}
 			
 }
--- media/init.php
+++ media/init.php
@@ -1,9 +1,23 @@
 <?php
 
-Route::set('media', 'media(/<action>)',
+if(Kohana::$environment >= Kohana::DEVELOPMENT){
+	Route::set('media', '<dir>(/<file>)',
+			array(
+				'dir' => '(css|images|js)',
+				'file' => '.*'
+		))
+		->defaults(array(
+			'controller' => 'media',
+			'action'     => 'get',
+			'directory'  => NULL,
+			'file'       => NULL,
+		));
+}else{
+	Route::set('media', 'media(/<action>)',
     array(
         'action'    => '(index|move)',
     ))->defaults(array(
         'controller'=> 'media',
         'action'    => 'index',
     ));
+}
