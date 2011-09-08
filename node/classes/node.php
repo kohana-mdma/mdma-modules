57b18ba05a26e7d57b744ead9324563bf2179fab
Big_Shark <Big_Shark@mail.ru>
2011-09-08 15:51:47 +1100

    new route

--- node/classes/node.php
+++ node/classes/node.php
@@ -4,4 +4,21 @@ class Node{
 
 	public static $current = NULL;
 
+	public static function route($uri) {
+		if( ! $uri)$uri = 'index';
+		$node = ORM::factory('node')->where('url_path', '=', '/'.trim($uri, '/').'/')->find();
+		if($node->type == 'folder'){
+			$children = $node->children->find_all();
+			if(count($children)){
+				$node = current($children);
+			}else{
+				$node = ORM::factory('node');
+			}
+		}
+		if($node->loaded() and $node->request){
+			Node::$current = $node;
+			$array = Request::process_uri($node->request, array_diff_key(Route::all(), array('node'=>NULL)));
+			return $array['params']; 		
+		}
+	}
 }
