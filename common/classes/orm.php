c2e0abc0063db30f822b6a4d264df2920d00a325
Big_Shark <Big_Shark@mail.ru>
2011-09-23 19:26:41 +1100

    hook http://dev.kohanaframework.org/issues/4177

--- common/classes/orm.php
+++ common/classes/orm.php
@@ -67,5 +67,40 @@ class ORM extends Kohana_ORM {
 		}
         return $array;
     }
+	
+	/**
+	 * Initializes the Database Builder to given query type
+	 *
+	 * @param  integer $type Type of Database query
+	 * @return ORM
+	 */
+	protected function _build($type)
+	{
+		// Construct new builder object based on query type
+		switch ($type)
+		{
+			case Database::SELECT:
+				$this->_db_builder = DB::select();
+			break;
+			case Database::UPDATE:
+				$this->_db_builder = DB::update(array($this->_table_name, $this->_object_name));
+			break;
+			case Database::DELETE:
+				$this->_db_builder = DB::delete($this->_table_name);
+		}
+
+		// Process pending database method calls
+		foreach ($this->_db_pending as $method)
+		{
+			$name = $method['name'];
+			$args = $method['args'];
+
+			$this->_db_applied[$name] = $name;
+
+			call_user_func_array(array($this->_db_builder, $name), $args);
+		}
+
+		return $this;
+	}
 }
 
